ca7f7093a941b32b86e87b628707ff8f336a548c
asavva <savva.al@gmail.com>
2023-06-28 17:04:55 +0200

    update

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-coucou 2 !!
+
+<!doctype html>
+<html lang="fr">
+  <head>
+    <title>Intégration web - UX design | Alexia SAVVA</title>
+
+    <meta name="description" content="Intégration web, développement font-end spécialisé en webdesign, recherche sur l'interface et l'experience utilisateur UI/UX design - Alexia Savva, Paris Ile de France">
+
+    <meta name="keywords" content="integration web, front end, integrateur, developpeur front end, webdesign, ux design, ui design, user experience">
+    <meta name="author" content="Alexia Savva">
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width"/>
+    <meta name="HandheldFriendly" content="true">
+    <meta name="MobileOptimized" content="320">
+
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    
+    <link rel="stylesheet/css" type="text/css" href="styles/styles.css">
+
+    <script src="assets/js/less-3.0.1.js" type="text/javascript"></script>
+    <link rel="shortcut icon" href="assets/img/favicon-panda.png">
+
+    <!--
+    <meta name="google-site-verification" content="67w_gB15PYY4jUTR4sE-5r98CJnIsH5Ij8Ffj6XXUTs">
+    <script src='https://www.google.com/recaptcha/api.js'></script>
+    -->
+  </head>
+  
+  <body>
+Blop toto !
+
+  </body>
+</html>
