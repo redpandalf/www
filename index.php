b97e6c5e22b0df140212dbffdce455290de12482
asavva <savva.al@gmail.com>
2023-06-28 17:19:32 +0200

    update

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     
-    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    <link rel="stylesheet" type="text/css" href="includes/styles/styles.css">
 
     <link rel="shortcut icon" href="../assets/img/favicon-panda.png">
 
